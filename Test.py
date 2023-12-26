3367dbf3155d988fa65a3857a5f54823a9092f02
yiyeon0914 <154914604+yiyeon0914@users.noreply.github.com>
2023-12-26 11:13:23 -0800

    Added response to the file

--- Test.py
+++ Test.py
@@ -1,1 +1,32 @@
-print('hello yong jin')
+print('Hello yong jin, are you ready?')
+answer=input('Are you ready to play the Quiz ? (yes/no) :')
+score=0
+total_questions=3
+ 
+if answer.lower()=='yes':
+    answer=input('Question 1: Where do you work?')
+    if answer.lower()=='samsung':
+        score += 1
+        print('correct')
+    else:
+        print('Wrong Answer :(')
+ 
+ 
+    answer=input('Question 2: Who is your son? ')
+    if answer.lower()=='Yiyeon':
+        score += 1
+        print('correct')
+    else:
+        print('Wrong Answer :(')
+ 
+    answer=input('Question 3: Where do you live?')
+    if answer.lower()=='San Jose':
+        score += 1
+        print('correct')
+    else:
+        print('Wrong Answer :(')
+ 
+print('Thankyou for Playing this small quiz game, you attempted',score,"questions correctly!")
+mark=(score/total_questions)*100
+print('Marks obtained:',mark)
+print('BYE!')

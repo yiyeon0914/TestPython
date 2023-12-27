b51edcef2d40274d848b5eb0702ce4f160f7e160
yiyeon0914 <154914604+yiyeon0914@users.noreply.github.com>
2023-12-27 12:52:12 -0800

    Less code and for the else situation.

--- Test.py
+++ Test.py
@@ -2,7 +2,14 @@ print('Hello yong jin, are you ready?')
 answer=input('Are you ready to play the Quiz ? (yes/no) :')
 score=0
 total_questions=3
- 
+
+def update_score(is_correct) :
+    if is_correct == True :
+        score +=1
+
+    else : 
+        print('Wrong answer')
+
 if answer.lower()=='yes':
     answer=input('Question 1: Where do you work?')
     if answer.lower()=='samsung':
@@ -10,23 +17,26 @@ if answer.lower()=='yes':
         print('correct')
     else:
         print('Wrong Answer :(')
- 
- 
-    answer=input('Question 2: Who is your son? ')
-    if answer.lower()=='Yiyeon':
+
+    answer=input('Question 2: Where do you come from? ')
+    if answer.lower()=='korea':
         score += 1
         print('correct')
-    else:
-        print('Wrong Answer :(')
- 
+
     answer=input('Question 3: Where do you live?')
-    if answer.lower()=='San Jose':
+    if answer.lower()=='san_jose':
         score += 1
         print('correct')
-    else:
-        print('Wrong Answer :(')
  
-print('Thankyou for Playing this small quiz game, you attempted',score,"questions correctly!")
+def update_score(is_correct) :
+    if is_correct == True :
+        score +=1
+
+    else : 
+        print('Wrong answer')
+
+
+print('Thank you for Playing this small quiz game, you attempted',score,"questions correctly!")
 mark=(score/total_questions)*100
 print('Marks obtained:',mark)
 print('BYE!')

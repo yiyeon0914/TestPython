d4cc8f1f22e9719c42b3b7499980df8a255025c6
yiyeon0914 <154914604+yiyeon0914@users.noreply.github.com>
2023-12-27 22:45:42 -0800

    Updated code to use fuctions

--- Test.py
+++ Test.py
@@ -4,37 +4,23 @@ score=0
 total_questions=3
 
 def update_score(is_correct) :
+    global score
+    
     if is_correct == True :
         score +=1
-
+        print('correct')    
     else : 
         print('Wrong answer')
 
 if answer.lower()=='yes':
     answer=input('Question 1: Where do you work?')
-    if answer.lower()=='samsung':
-        score += 1
-        print('correct')
-    else:
-        print('Wrong Answer :(')
+    update_score(answer.lower()=='samsung')
 
     answer=input('Question 2: Where do you come from? ')
-    if answer.lower()=='korea':
-        score += 1
-        print('correct')
+    update_score(answer.lower()=='korea')
 
     answer=input('Question 3: Where do you live?')
-    if answer.lower()=='san_jose':
-        score += 1
-        print('correct')
- 
-def update_score(is_correct) :
-    if is_correct == True :
-        score +=1
-
-    else : 
-        print('Wrong answer')
-
+    update_score(answer.lower()=='san jose')
 
 print('Thank you for Playing this small quiz game, you attempted',score,"questions correctly!")
 mark=(score/total_questions)*100
